3403be26d5dffa1d1a732e36f4fb28205bc55304
Jason Gors <jason.gors.work@gmail.com>
2016-01-27 11:34:55 -0800

    RF: add create camus specific dir

--- run_camus.sh
+++
@@ -1,3 +0,0 @@
-cd /usr/local/hadoop/etc/hadoop/
-/usr/local/hadoop jar camus-example-0.1.0-SNAPSHOT-shaded.jar com.linkedin.camus.etl.kafka.CamusJob -P /usr/local/camus/camus-example/src/main/resources/camus.properties
-cd -
