92cd01ae1ff370ab835eee1a319ca1b8de8b65fb
Jason Gors <jason.gors.work@gmail.com>
2016-01-27 11:01:31 -0800

    NF: add file to dynamically change retention time for all topic cities

---
+++ kafka/change_topic_retention_time.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+#----------------------------------------------------------------
+# Author: Jason Gors <jasonDOTgorsATgmail>
+# Creation Date: 01-27-2016
+# Purpose: to change how long kafka keeps stuff in each topic
+# http://stackoverflow.com/questions/29129222/changing-kafka-rentention-period-during-runtime
+# http://www.convertunits.com/from/month/to/millisecond
+#----------------------------------------------------------------
+import subprocess
+import os, sys
+parent_dir = os.path.dirname(os.getcwd())
+sys.path.append(parent_dir)
+import envir_vars
+
+ms_in_a_month = 2629746000
+
+for city in envir_vars.cities_lat_and_long.keys():
+    cmd = '$KAFKA_HOME/bin/kafka-topics.sh --zookeeper localhost:2181'
+    cmd += ' --alter --topic {} --config retention.ms={}'.format(city, ms_in_a_month)
+    sproc = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    out, err = sproc.communicate()
+    if err:
+        print "ERROR:  something didn't work trying to change retention with:\n{}".format(cmd)
