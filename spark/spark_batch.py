d9edc81094efe819cfda75b9f52a7b42d522ce2d
jason gors <jason.gors.work@gmail.com>
2016-01-30 15:53:30 +0000

    WIP: deduped alerts across rows

--- spark/spark_batch.py
+++ spark/spark_batch.py
@@ -6,6 +6,7 @@
 # Purpose:
 #----------------------------------------------------------------
 
+import json
 from pyspark import SparkConf, SparkContext, SQLContext
 import pyspark
 
@@ -32,9 +33,7 @@ sc = SQLContext(sc)
 
 
 
-
-
-import json
+# NOTE START FROM HERE
 from pyspark import SQLContext
 # from pyspark.sql import SQLContext    # same thing
 
@@ -42,15 +41,18 @@ pub_dns = 'ec2-52-89-106-226.us-west-2.compute.amazonaws.com'   # this is the ma
 topic = 'san-fran-small-wo-newline'
 
 # For example:  /camus/topics/san-fransisco/hourly/2016/01/21/10
-# data_path_on_hdfs = '/camus/topics/*/*/*/*/*/*'
+# data_path_on_hdfs = '/camus/topics/*/*/*/*/*/*'     # everything
 # data_path_on_hdfs = '/camus/topics/san-fransisco/*/*/*/*/*'
+# data_path_on_hdfs = '/camus/topics/chicago/hourly/2016/01/28/06/*'
 data_path_on_hdfs = '/camus/topics/{}/*/*/*/*/*'.format(topic)
 
 data_path = "hdfs://{}:9000/{}".format(pub_dns, data_path_on_hdfs)
 
 
+
 #################################
 # NOTE not using this
+'''
 # f = sc.textFile("hdfs://<public_dns>:9000/user/test.txt")
 rdd = sc.textFile(data_path)#.cache()
 # rdd_dicts = rdd.map(json.loads)
@@ -65,15 +67,18 @@ alerts = rdd_dicts.map(lambda x: x['alerts']).filter(lambda x: x=='POLICE')
 # police = alerts.filter(lambda x: x['type']=='POLICE')
 # print police.take(1)
 print alerts.take(1)
+'''
 #################################
 
 
 
 ##############################
-# NOTE coded this w/ Austin -- NOTE works!!!!
+# works
 sc_sql = SQLContext(sc)
-# json_data = sc_sql.jsonFile(data_path)
 json_data = sc_sql.read.json(data_path)
+# json_data = sc_sql.jsonFile(data_path)
+
+# json_data.printSchema()
 
 # j_first = json_data.take(1)
 # type(j_first[0])  # each entry is a pyspark.sql.types.Row object
@@ -85,17 +90,84 @@ json_data = sc_sql.read.json(data_path)
         # # return i.latitude
         # print alert
 
+# def func(a):
+    # pass
+# something = json_data.map(lambda a: (a.latitude, a.longitude, a.type), x.time_stamp)
+
+
+# works
 def get_alerts(row):
     locations = []
     for alert in row.alerts:
         if alert.type == 'POLICE':
             locations.append((alert.latitude, alert.longitude))
-    return (row.time_stamp, len(locations), str(locations))     # FIXME don't do this hacky locations str conversion -- figure out how to pass collections into cassandra
+    return (row.time_stamp, len(locations), locations)
 
 result = json_data.map(get_alerts)
 
-# r1 = result.take(1)
-# type(r1) is list
+
+
+
+
+
+
+
+
+# data_path_on_hdfs = '/camus/topics/chicago/hourly/2016/01/28/06/*'
+# data_path_on_hdfs = '/waze_data/topics/{}/'.format(topic)
+data_path_out_hdfs = '/testing/{}/'.format(topic)
+hdfs_outpath = "hdfs://{}:9000/{}".format(pub_dns, data_path_out_hdfs)
+
+def get_alerts(row):
+    alerts = []
+    for alert in row.alerts:
+        # if alert.type == 'POLICE':
+        # TODO save back to hdfs here and then read those hdfs files back in further down
+        # in this script.  so each line would look like:
+        # {'time_stamp': 1234345, 'lat': 2342, 'lon': 1234, 'type': 'police', 'subtype': 'hidden_police'}
+        # row_reformatted = [('time_stamp', row.time_stamp),
+                            # ('lat', alert.latitude), ('lon', alert.longitude),
+                            # ('type', alert.type), ('subtype', alert.subType)]
+        row_reformatted = {'time_stamp': row.time_stamp,
+                           'lat': alert.latitude, 'lon': alert.longitude,
+                           # 'lat_and_lon': (alert.latitude, alert.longitude),
+                           'type': alert.type, 'subtype': alert.subType}
+        alerts.append(row_reformatted)
+        # df = sc_sql.createDataFrame(row_reformatted)
+        # df.saveAsTextFile(hdfs_outpath)
+        # locations.update({(alert.latitude, alert.longitude): row.time_stamp})
+        # print row_reformatted
+    return alerts
+
+
+result = json_data.map(get_alerts)
+rfm = result.flatMap(lambda row: [alert for alert in row])
+# rfm.saveAsTextFile(hdfs_outpath)  # this would be what i'd want NOTE it works
+
+# output = sc_sql.createDataFrame(rfm)
+# result.saveAsTextFile(hdfs_outpath)
+
+rfm_df = rfm.toDF()
+rfm_df.count()
+# rfm_df_dedup_1 = rfm_df.dropDuplicates(['lat_and_lon'])
+rfm_df_dedup_2 = rfm_df.dropDuplicates(['lat', 'lon'])
+
+
+# maybe something like this?
+# result = json_data.select(json_data.alerts)
+# result_new = result.flatMap(lambda r: r)
+
+
+# def func(row):
+    # d_merged = {}
+    # for d in row:
+        # d_merged.update(d)
+
+# result = json_data.map(get_alerts).map(func)
+
+
+
+
 ##############################
 
 
@@ -160,7 +232,7 @@ result = json_data.map(get_alerts)
 # do this from inside python:
 # https://sites.google.com/a/insightdatascience.com/dataengineering/devsetups
 
-def agg_to_cassandra_part(agg):
+def hand_off_to_cassandra(agg):
     from cassandra.cluster import Cluster
     if agg:
         cluster = Cluster(['52.89.106.226', '52.89.118.67', '52.34.130.78', '52.89.11.71'])
@@ -168,16 +240,17 @@ def agg_to_cassandra_part(agg):
         # session = cluster.connect('san_fran_small_wo_newline_police')
         # session = cluster.connect('sanfranpolice')
         session = cluster.connect('waze')
-        for agg_item in agg:
+        for a in agg:
             session.execute(
                     # 'INSERT INTO sliding_window_batch (monitor, ts_start, time_total) VALUES (%s, %s, %s)', (agg_item[0], 0, agg_item[1]))
                     # 'INSERT INTO police (time_stamp, num_police, locations) VALUES (%s, %s, %s)', (agg_item[0], agg_item[1], agg_item[2]))
                     # 'INSERT INTO sanfranpolice (time, count) VALUES (%s, %s)', (agg_item[0], agg_item[1]))    # NOTE works
-                    'INSERT INTO sanfranpolice (time, count, locations) VALUES (%s, %s, %s)', (agg_item[0], agg_item[1], agg_item[2]))
+                    'INSERT INTO sanfranpolice (time, count, locations) VALUES (%s, %s, %s)', (a[0], a[1], a[2]))
                     # 'INSERT INTO police (time_stamp, num_police, locations) VALUES (%s, %s, %s)'.format(agg_item[0], agg_item[1], agg_item[2]))
         session.shutdown()
         cluster.shutdown()
-result.foreachPartition(agg_to_cassandra_part)
+
+result.foreachPartition(hand_off_to_cassandra)
 
 '''
 from cassandra.cluster import Cluster
@@ -187,3 +260,6 @@ session.execute(
         'INSERT INTO sanfranpolice (time_stamp, num_police, locations) VALUES (%s, %s, %s)', (agg_item[0], agg_item[1], agg_item[2]))
 '''
 
+'''
+create a group for each city
+'''
