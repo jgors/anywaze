63cf996d4dc404d60f48c692e21cecfdec8cf8b6
jason gors <jason.gors.work@gmail.com>
2016-01-31 02:58:05 +0000

    WIP: boilerplate code to grab data out of kafka

--- spark/spark_stream.py
+++ spark/spark_stream.py
@@ -2,71 +2,58 @@
 
 #----------------------------------------------------------------
 # Author: Jason Gors <jasonDOTgorsATgmail>
-# Creation Date: 01-28-2016
+# Creation Date: 01-30-2016
 # Purpose:
 #----------------------------------------------------------------
 
+"""Run with:
+$SPARK_HOME/bin/spark-submit --packages org.apache.spark:spark-streaming-kafka_2.10:1.6.0 spark_stream_test.py
+"""
 
-import sys
-import json
-from pyspark import SparkContext, SparkConf
-from pyspark.streaming.kafka import KafkaUtils
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
-from pyspark.sql import SQLContext, Row
-from pyspark.sql.types import *
-# from cassandra.cluster import Cluster
-# from cassandra import ConsistencyLevel
+from pyspark.streaming.kafka import KafkaUtils
+import json, datetime
+import sys
 
 
 
-def process(rdd):
-    # rdd.foreachPartition(lambda record: write_into_cassandra(record))
-    print rdd
-
-topic = "nyc"
-# topic = 'san-fran-small-wo-newline'
-
-# spark_cluster_master = 'spark://ec2-54-69-163-111.us-west-2.compute.amazonaws.com:7077'
-appname_for_spark_ui = 'waze_data'
-# sc = SparkContext(spark_cluster_master, app_name_for_spark_ui)
-sc = SparkContext(appName=appname_for_spark_ui)
-
-ssc = StreamingContext(sc, 2)
-
-kafka_machines = ['52.89.106.226', '52.89.118.67', '52.34.130.78', '52.89.11.71']
-# spark_machines = ['54.69.163.111', '54.69.140.82', '54.69.108.204', '54.69.162.20']
-zkQuorum = ','.join([m + ':2181' for m in kafka_machines])
-kvs = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-consumer", {topic: 1})
-lines = kvs.map(lambda x: x[1])
+if __name__ == "__main__":
+    sc = SparkContext(appName="PythonStreamingKafka")
+    ssc = StreamingContext(sc, 2)   # every 2 seconds
 
 
+    # topic = 'chicago'
+    kafka_machines = ['52.89.106.226', '52.89.118.67', '52.34.130.78', '52.89.11.71']
+    zkQuorum = ','.join([m + ':2181' for m in kafka_machines])
+    # zkQuorum = '52.89.106.226:2181,52.89.118.67:2181,52.34.130.78:2181,52.89.11.71:2181'
+    topics = {'chicago': 1}
+    kafkaStream = KafkaUtils.createStream(ssc, zkQuorum, "GroupNameDoesntMatter", topics) #{topic: 1})
+    # lines = kafkaStream.map(lambda x: json.loads(x[1]))
+    lines = kafkaStream.map(lambda x: x[1].strip())
+    # lines.pprint()
 
-# sc_sql = SQLContext(sc)
-# json_data = sc_sql.read.json(lines)
+    alertofinterest = 'POLICE'
+    output = lines.map(lambda l: json.loads(l)["alerts"])
+    output.pprint()
+    print
 
-# def get_alerts(row):
-    # locations = []
-    # for alert in row.alerts:
-        # locations.append({alert.latitude, alert.longitude})
-    # # return (row.time_stamp, len(locations), str(locations))
-    # return locations
-
-# result = json_data.map(get_alerts)
-alertofinterest = 'POLICE'
-output = lines.map(lambda l: json.loads(l)["alerts"])#\
-# output = lines.filter(lambda l: alertofinterest in json.loads(l)["alerts"]['type'])#\
-    # .filter(lambda l: len(json.loads(l)["place"]["name"]) > 0 )\
-    # .filter(lambda l: len(json.loads(l)["place"]["country_code"]) > 0)\
-    # .map(lambda l: ( (json.loads(l)["place"]["name"], json.loads(l)["place"]["country_code"] ), 1))\
-    # .reduceByKey(lambda a,b: a+b)
-
-output.pprint()
-
-# output.foreachRDD(citycount_to_cassandra)
+    ssc.start()
+    ssc.awaitTermination()
 
 
 
 
-ssc.start()
-ssc.awaitTermination()
+    '''
+    zkQuorum, topic = '52.89.106.226', 'nyc'
+    kvs = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-consumer", {topic: 1})
+    lines = kvs.map(lambda x: x[1])
+    # counts = lines.flatMap(lambda line: line.split(" ")) \
+        # .map(lambda word: (word, 1)) \
+        # .reduceByKey(lambda a, b: a+b)
+    alerts = lines.map(lambda line: line.alerts)
+    alerts.pprint()
+
+    ssc.start()
+    ssc.awaitTermination()
+    '''
