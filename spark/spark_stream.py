e0c1c7469a2dfbaeb80c58f89519052eb12e9362
jason gors <jason.gors.work@gmail.com>
2016-01-31 03:46:05 +0000

    WIP: tuned up streaming to get the useful info at specific time increments

--- spark/spark_stream.py
+++ spark/spark_stream.py
@@ -7,8 +7,13 @@
 #----------------------------------------------------------------
 
 """Run with:
-$SPARK_HOME/bin/spark-submit --packages org.apache.spark:spark-streaming-kafka_2.10:1.6.0 spark_stream_test.py
+sh spark_stream_exe.sh
 """
+
+import sys, os
+parent_dir = os.path.dirname(os.getcwd())
+sys.path.append(parent_dir)
+import envir_vars
 
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
@@ -20,23 +25,43 @@ import sys
 
 if __name__ == "__main__":
     sc = SparkContext(appName="PythonStreamingKafka")
-    ssc = StreamingContext(sc, 2)   # every 2 seconds
+    # ssc = StreamingContext(sc, 2)   # every 2 seconds
+    ssc = StreamingContext(sc, 1)   # every 2 seconds
 
 
-    # topic = 'chicago'
+    # topics = {'chicago': 1, 'nyc': 1}
+    topics = {city: 1 for city in envir_vars.cities_lat_and_long.keys()}
+
+
+    # zkQuorum = '52.89.106.226:2181,52.89.118.67:2181,52.34.130.78:2181,52.89.11.71:2181'
     kafka_machines = ['52.89.106.226', '52.89.118.67', '52.34.130.78', '52.89.11.71']
     zkQuorum = ','.join([m + ':2181' for m in kafka_machines])
-    # zkQuorum = '52.89.106.226:2181,52.89.118.67:2181,52.34.130.78:2181,52.89.11.71:2181'
-    topics = {'chicago': 1}
-    kafkaStream = KafkaUtils.createStream(ssc, zkQuorum, "GroupNameDoesntMatter", topics) #{topic: 1})
+
+    kafkaStream = KafkaUtils.createStream(ssc, zkQuorum, "GroupNameDoesntMatter", topics)
     # lines = kafkaStream.map(lambda x: json.loads(x[1]))
-    lines = kafkaStream.map(lambda x: x[1].strip())
+    lines = kafkaStream.map(lambda x: x[1])
     # lines.pprint()
 
-    alertofinterest = 'POLICE'
-    output = lines.map(lambda l: json.loads(l)["alerts"])
-    output.pprint()
-    print
+
+    def get_useful_info(alerts):
+        d = {}  # {(lat, lng): [type, subType]
+        for alert in alerts:
+            d.update({(alert['latitude'], alert['longitude']): [alert['type'], alert['subType']]})
+        return d
+
+    try:
+        # alertofinterest = 'POLICE'
+        # output = lines.map(lambda l: [alert for alert in json.loads(l)["alerts"] if alert['type'] == alertofinterest])
+        # output = lines.map(lambda l: json.loads(l)["city"])
+        # output.pprint()
+        alerts_now = {}
+        output = lines.map(lambda l: json.loads(l)["alerts"])   # a list of dicts
+        alerts_now = output.map(get_useful_info)
+        alerts_now.pprint()
+        print
+    except Exception as e:
+        # TODO log these to file
+        print "ERROR: something bad happened", e
 
     ssc.start()
     ssc.awaitTermination()
