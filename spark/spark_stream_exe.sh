4cc3a1fddf470cc9da1fed62854f4c76d5ab5a0a
jason gors <jason.gors.work@gmail.com>
2016-02-02 03:51:42 +0000

    RF: up'ed the memory for spark_stream

--- spark/spark_stream_exe.sh
+++ spark/spark_stream_exe.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-$SPARK_HOME/bin/spark-submit --executor-memory 10000M --driver-memory 10000M --packages org.apache.spark:spark-streaming-kafka_2.10:1.6.0 spark_stream.py
+$SPARK_HOME/bin/spark-submit --executor-memory 14000M --driver-memory 14000M --packages org.apache.spark:spark-streaming-kafka_2.10:1.6.0 spark_stream.py
