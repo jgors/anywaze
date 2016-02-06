5319a1367b0451ec4cd6470547cab4f49c9f9858
Jason Gors <jason.gors.work@gmail.com>
2016-02-06 00:40:49 +0000

    WIP: dirty push to get stuff on gh

--- flask/app/views.py
+++ flask/app/views.py
@@ -55,8 +55,9 @@ def slides():
 pg = 'date_type'
 @app.route('/{}'.format(pg))
 def date_type():
-    return render_template("{}.html".format(pg))
     # return render_template("dots.html")
+    return render_template("{}.html".format(pg))
+
 '''
 @app.route("/{}".format(pg), methods=['POST'])
 def date_type_post():
@@ -78,8 +79,47 @@ def date_type_post():
     return render_template("{}_op.html".format(pg), output=jsonresponse)
 '''
 
+# @app.route("/{}".format(pg), methods=['POST'])
+# def date_type_post():
+    # type_id = request.form["type_id"]
+    # date = request.form["date"]
+
+    # # type entered is in type_id and date selected in dropdown is in date variable
+    # stmt = "SELECT * FROM date_and_type WHERE date=%s AND type=%s"
+    # response = session.execute(stmt, parameters=[date, type_id.upper()])
+
+    # '''
+    # data = {
+        # 'sample': ['1st', '2nd', '1st', '2nd', '1st', '2nd'],
+        # 'interpreter': ['python', 'python', 'pypy', 'pypy', 'jython', 'jython'],
+        # 'timing': [-2, 5, 12, 40, 22, 30],
+    # }
+    # # x-axis labels pulled from the interpreter column, stacking labels from sample column
+    # dots = Dot(data, values='timing', label='interpreter',
+            # group='sample', agg='mean',
+            # title="Python Interpreter Sampling",
+            # legend='top_right', width=600)
+    # output_file("app/templates/dots.html", title="dots.py example")
+    # '''
+
+    # response_list = []
+    # for val in response:
+        # response_list.append(val)
+    # jsonresponse = [{"date": x.date,
+                     # "type": x.type,
+                     # "city": x.city,
+                     # "count": x.count,
+                    # } for x in response_list]
+    # print jsonresponse
+    # return render_template("{}_op.html".format(pg), output=jsonresponse)
+
+
 @app.route("/{}".format(pg), methods=['POST'])
 def date_type_post():
+    ''' type_id: eg. 'accident', 'police', etc
+        date: eg. '2016-1-27'
+    '''
+
     type_id = request.form["type_id"]
     date = request.form["date"]
 
@@ -87,30 +127,37 @@ def date_type_post():
     stmt = "SELECT * FROM date_and_type WHERE date=%s AND type=%s"
     response = session.execute(stmt, parameters=[date, type_id.upper()])
 
-    '''
-    data = {
-        'sample': ['1st', '2nd', '1st', '2nd', '1st', '2nd'],
-        'interpreter': ['python', 'python', 'pypy', 'pypy', 'jython', 'jython'],
-        'timing': [-2, 5, 12, 40, 22, 30],
-    }
-    # x-axis labels pulled from the interpreter column, stacking labels from sample column
-    dots = Dot(data, values='timing', label='interpreter',
-            group='sample', agg='mean',
-            title="Python Interpreter Sampling",
-            legend='top_right', width=600)
-    output_file("app/templates/dots.html", title="dots.py example")
-    '''
+    type_id = str(type_id)
+    date = str(date)
 
-    response_list = []
-    for val in response:
-        response_list.append(val)
-    jsonresponse = [{"date": x.date,
-                     "type": x.type,
-                     "city": x.city,
-                     "count": x.count,
-                    } for x in response_list]
-    print jsonresponse
-    return render_template("{}_op.html".format(pg), output=jsonresponse)
+    chartID = 'my_chart'
+    kwargs = dict(
+        chart = {"renderTo": chartID, "type": 'column', "height": '600px'},
+
+        credits = {"text": 'jason gors', "href": 'http://jgors.com'},
+        title = {"text": '{} reports'.format(type_id)},
+        subtitle = {"text": date},
+        yAxis = {"min": 0, "title": {"text": 'Number Reported'}},
+        tooltip = {"pointFormat": '%s on %s: <b>{point.y:.0f}</b>' % (type_id, date)},
+    )
+
+    series_name = type_id
+    series_data = []
+    for r in response:
+        series_data.append([str(r.city), r.count])
+    series_data = sorted(series_data, reverse=True, key=lambda cities_data: cities_data[1])
+
+    kwargs.update(series_name=series_name, series_data=series_data, chartID=chartID)
+    # print kwargs
+
+    # NOTE for some reason this stuff breaks it:
+    # {% extends "base.html" %}
+    # {% block date_type %}
+    # if that stuff is removed, then the graph works
+
+    # return render_template('graph.html', **kwargs)
+    return render_template("{}_op.html".format(pg), **kwargs)
+
 
 
 
