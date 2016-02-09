dd888dda2a1b6dfd3ca571905e96d209ca8c445f
jason gors <jason.gors.work@gmail.com>
2016-02-09 22:47:36 +0000

    WIP+RF: made proc script support multiple procing tasks; NOTE needs to be RF'ed to clean up this garbage

--- spark/spark_batch_proc.py
+++ spark/spark_batch_proc.py
@@ -16,16 +16,14 @@ from envir_vars import hdfs_data_path, storage_cluster_ips, cassandra_keyspace
 from pyspark import SparkConf, SparkContext, SQLContext
 from cassandra.cluster import Cluster
 
+# FIXME this should be an arg passed into the script rather than hardcoded like this
+data_to_proc = 'heatmaps'
+
 
 # NOTE spark_master_hostname  ==  ip-172-31-1-85
 # this way                                  {spark_master_hostname}
 # $SPARK_HOME/bin/spark-submit --master spark://ip-172-31-1-85:7077 --packages TargetHolding/pyspark-cassandra:0.2.7 --executor-memory 14000M --driver-memory 14000M spark_batch_proc.py
 
-''' from sesha
-spark-submit
---master masterip
---packages org.apache.spark:spark-streaming-kafka_2.10:1.5.2,TargetHolding:pyspark-cassandra:0.2.4
-'''
 
 
 # NOTE this would be for running spark stand-alone (not spark interactive)
@@ -77,23 +75,57 @@ def make_data_for_cassandra(row):
     return new_row
 
 
-rdd_data = df.map(make_data_for_cassandra)
-new_df = rdd_data.toDF()
 
-###  -->                             key is a tuple          and initially 1 for each thing      then add(/reduce) up all things that have similar keys
-map_reduced_date_and_event = new_df.map(lambda r: ((r.year_month_day, r.city, r.type), 1)).reduceByKey(lambda a,b: a+b)
-result_date_and_type = sorted(map_reduced_date_and_event.collect())
-# result_date_and_type[0]  ==  ((u'2016-01-25', u'atlanta', u'ACCIDENT'), 97)
+if data_to_proc == 'date_and_type':
+    rdd_data = df.map(make_data_for_cassandra)
+    new_df = rdd_data.toDF()
+
+    ###  -->                             key is a tuple          and initially 1 for each thing      then add(/reduce) up all things that have similar keys
+    map_reduced_date_and_event = new_df.map(lambda r: ((r.year_month_day, r.city, r.type), 1)).reduceByKey(lambda a,b: a+b)
+    result_date_and_type = sorted(map_reduced_date_and_event.collect())
+    # result_date_and_type[0]  ==  ((u'2016-01-25', u'atlanta', u'ACCIDENT'), 97)
 
 
 # map_reduced_date_and_city = new_df.map(lambda r: ((r.city, r.type, r.year_month_day, r.hour), 1)).reduceByKey(lambda a,b: a+b)
 # result_date_and_city = sorted(map_reduced_date_and_city.collect())
 # result_date_and_city[0]  ==  ((u'atlanta', u'ACCIDENT', u'2016-01-25', 10), 1)
 
+elif data_to_proc == 'date_and_city':
+    rdd_data = df.map(make_data_for_cassandra)
+    new_df = rdd_data.toDF()
 
-map_reduced_date_and_city = new_df.map(lambda r: ((r.year_month_day, r.city, r.type, r.subtype), 1)).reduceByKey(lambda a,b: a+b)
-result_date_and_city = sorted(map_reduced_date_and_city.collect())
-# result_date_and_city[1]  ==  (( u'2016-01-25', u'atlanta', u'ACCIDENT', u'ACCIDENT_MAJOR'), 16)
+    map_reduced_date_and_city = new_df.map(lambda r: ((r.year_month_day, r.city, r.type, r.subtype), 1)).reduceByKey(lambda a,b: a+b)
+    result_date_and_city = sorted(map_reduced_date_and_city.collect())
+    # result_date_and_city[1]  ==  (( u'2016-01-25', u'atlanta', u'ACCIDENT', u'ACCIDENT_MAJOR'), 16)
+
+
+
+
+
+def make_heatmaps_data_for_cassandra(row):
+    year_month_day, hour_minute = row.datetime.split(' ')
+    hour = int(hour_minute.split(':')[0])
+
+    new_row = {'city': row.city,
+               'type': row.type,
+               'subtype': row.subtype,
+               'weekday': row.weekday,
+               'year_month_day': year_month_day,
+               'hour': hour,
+               'lat': row.lat,
+               'lng': row.lng,
+              }
+    return new_row
+
+
+if data_to_proc == 'heatmaps':
+    rdd_data = df.map(make_heatmaps_data_for_cassandra)
+    new_df = rdd_data.toDF()
+    # result_heatmaps = sorted(new_df.collect())
+    result_heatmaps = new_df.collect()
+
+
+
 
 
 ##############################################
@@ -102,22 +134,6 @@ result_date_and_city = sorted(map_reduced_date_and_city.collect())
 # cqlsh> CREATE KEYSPACE waze WITH replication = {'class': 'SimpleStrategy', 'replication_factor':3};
 # cqlsh> USE waze ;
 # cqlsh:waze> CREATE TABLE ... (var1 timestamp, var2 list, var3 int, PRIMARY KEY (var1, var3));
-'''
-want daily aggregate for each type (POLICE) for each city (LA) for each date (1-11-16)
-CREATE TABLE date_aggregation (
-    date timestamp,
-    event_type text
-    city text,
-    lat int,
-    lon int
-    PRIMARY KEY ((date, event_type))
-);
-SELECT * FROM date_aggregation WHERE date = '1-11-16'
-                           AND event_type = 'POLICE'
-
-within a date partition, rows will be ordered by event_type,
-all event_types for a given date will be on the same node
-'''
 
 '''
 def hand_off_to_cassandra(agg):
@@ -147,85 +163,134 @@ session = cluster.connect(cassandra_keyspace)
 # create keyspace (just do this once)
 # session.execute("""CREATE KEYSPACE waze WITH replication = {'class':'SimpleStrategy', 'replication_factor':3};""")
 
-# create table
-# date and type displayed for all cities -- #1 on sheet
-tablename = 'date_and_type'
-cmd = """CREATE TABLE {tablename} (
-    date text,
-    type text,
-    city text,
-    count int,
-    PRIMARY KEY ((type, date), city)
-);""".format(tablename=tablename)
+if data_to_proc == 'date_and_type':
+    # create table
+    # date and type displayed for all cities -- #1 on sheet
+    tablename = 'date_and_type'
+    cmd = """CREATE TABLE {tablename} (
+        date text,
+        type text,
+        city text,
+        count int,
+        PRIMARY KEY ((type, date), city)
+    );""".format(tablename=tablename)
 
-try:
-    session.execute(cmd)
-except Exception as e:
-    print e
+    try:
+        session.execute(cmd)
+    except Exception as e:
+        print e
 
-# https://datastax.github.io/python-driver/api/cassandra/cluster.html#cassandra.cluster.Session.prepare
-query = 'INSERT INTO {} (date, type, city, count) VALUES (?, ?, ?, ?)'.format(tablename)
-prepared = session.prepare(query)
-for r in result_date_and_type:
-    date, city, _type, count = r[0][0], r[0][1], r[0][2], r[1]
-    session.execute(prepared, (date, _type, city, count))
-
-
-
-
-# do the chart below instead of this one here
-# create table
-# date and city displayed for all types for all hours -- #2 on sheet
-'''
-tablename = 'date_and_city'
-cmd = """CREATE TABLE {tablename} (
-    date text,
-    type text,
-    city text,
-    hour int
-    count int,
-    PRIMARY KEY ((type, date, hour), city)
-);""".format(tablename=tablename)
-
-try:
-    session.execute(cmd)
-except Exception as e:
-    print e
-
-query = 'INSERT INTO {} (date, type, city, count) VALUES (?, ?, ?, ?)'.format(tablename)
-prepared = session.prepare(query)
-for r in result_date_and_type:
-    city, _type, date, count = r[0][0], r[0][1], r[0][2], r[1]
-    session.execute(prepared, (date, _type, city, count))
-'''
+    # https://datastax.github.io/python-driver/api/cassandra/cluster.html#cassandra.cluster.Session.prepare
+    query = 'INSERT INTO {} (date, type, city, count) VALUES (?, ?, ?, ?)'.format(tablename)
+    prepared = session.prepare(query)
+    for r in result_date_and_type:
+        date, city, _type, count = r[0][0], r[0][1], r[0][2], r[1]
+        session.execute(prepared, (date, _type, city, count))
 
 
 
 
-tablename = 'date_and_city'
-cmd = """CREATE TABLE {tablename} (
-    date text,
-    city text,
-    type text,
-    subtype text,
-    count int,
-    PRIMARY KEY ((date, city), type, subtype)
-);""".format(tablename=tablename)
+elif data_to_proc == 'date_and_city':
+    # elif 'date_and_city':
+    # do the chart below instead of this one here
+    # create table
+    # date and city displayed for all types for all hours -- #2 on sheet
+    '''
+    tablename = 'date_and_city'
+    cmd = """CREATE TABLE {tablename} (
+        date text,
+        type text,
+        city text,
+        hour int
+        count int,
+        PRIMARY KEY ((type, date, hour), city)
+    );""".format(tablename=tablename)
 
-try:
-    session.execute(cmd)
-except Exception as e:
-    print e
+    try:
+        session.execute(cmd)
+    except Exception as e:
+        print e
 
-query = 'INSERT INTO {} (date, city, type, subtype, count) VALUES (?, ?, ?, ?, ?)'.format(tablename)
-prepared = session.prepare(query)
-for r in result_date_and_city:
-    date, city, _type, subtype, count = r[0][0], r[0][1], r[0][2], r[0][3], r[1]
-    session.execute(prepared, (date, city, _type, subtype, count))
+    query = 'INSERT INTO {} (date, type, city, count) VALUES (?, ?, ?, ?)'.format(tablename)
+    prepared = session.prepare(query)
+    for r in result_date_and_type:
+        city, _type, date, count = r[0][0], r[0][1], r[0][2], r[1]
+        session.execute(prepared, (date, _type, city, count))
+    '''
 
 
 
+    tablename = 'date_and_city'
+    cmd = """CREATE TABLE {tablename} (
+        date text,
+        city text,
+        type text,
+        subtype text,
+        count int,
+        PRIMARY KEY ((date, city), type, subtype)
+    );""".format(tablename=tablename)
 
+    try:
+        session.execute(cmd)
+    except Exception as e:
+        print e
+
+    query = 'INSERT INTO {} (date, city, type, subtype, count) VALUES (?, ?, ?, ?, ?)'.format(tablename)
+    prepared = session.prepare(query)
+    for r in result_date_and_city:
+        date, city, _type, subtype, count = r[0][0], r[0][1], r[0][2], r[0][3], r[1]
+        session.execute(prepared, (date, city, _type, subtype, count))
+
+
+
+# elif data_to_proc == 'heatmaps':
+    # tablename = 'heatmaps'
+    # cmd = """CREATE TABLE {tablename} (
+        # city text,
+        # type text,
+        # subtype text,
+        # date text,
+        # hour int,
+        # lat float,
+        # lng float,
+        # PRIMARY KEY ((city, date, type), hour, lat, lng)
+    # );""".format(tablename=tablename)
+
+    # try:
+        # session.execute(cmd)
+    # except Exception as e:
+        # print e
+
+    # query = 'INSERT INTO {} (city, type, subtype, date, hour, lat, lng) VALUES (?, ?, ?, ?, ?, ?, ?)'.format(tablename)
+    # prepared = session.prepare(query)
+    # for r in result_heatmaps:
+        # session.execute(prepared, (r.city, r.type, r.subtype, r.year_month_day, r.hour, r.lat, r.lng))
+    # print "DONE"
+
+elif data_to_proc == 'heatmaps':
+    tablename = 'heatmaps2'
+    cmd = """CREATE TABLE {tablename} (
+        city text,
+        type text,
+        subtype text,
+        weekday text,
+        date text,
+        hour int,
+        lat float,
+        lng float,
+        PRIMARY KEY ((city, type, weekday), date, hour, lat, lng)
+    );""".format(tablename=tablename)
+
+    try:
+        session.execute(cmd)
+    except Exception as e:
+        print e
+
+    query = 'INSERT INTO {} (city, type, subtype, weekday, date, hour, lat, lng) VALUES (?, ?, ?, ?, ?, ?, ?, ?)'.format(tablename)
+    prepared = session.prepare(query)
+    for r in result_heatmaps:
+        session.execute(prepared, (r.city, r.type, r.subtype, r.weekday, r.year_month_day, r.hour, r.lat, r.lng))
+    print "DONE"
 
 
 session.shutdown()
