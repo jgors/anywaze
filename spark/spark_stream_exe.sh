ab75463d862e487ba36f6957ca82b6d4c7d2f1df
jason gors <jason.gors.work@gmail.com>
2016-02-09 22:50:00 +0000

    RF: add new lib for streaming data being saved to cassandra

--- spark/spark_stream_exe.sh
+++ spark/spark_stream_exe.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-$SPARK_HOME/bin/spark-submit --executor-memory 14000M --driver-memory 14000M --packages org.apache.spark:spark-streaming-kafka_2.10:1.6.0 spark_stream.py
+$SPARK_HOME/bin/spark-submit --packages org.apache.spark:spark-streaming-kafka_2.10:1.6.0,TargetHolding:pyspark-cassandra:0.2.7 --conf spark.cassandra.connection.host=52.89.106.226 --master spark://ip-172-31-1-85:7077 --executor-memory 14000M --driver-memory 14000M spark_stream.py
