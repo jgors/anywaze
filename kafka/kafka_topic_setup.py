eaa4f240e51b369b5ba6a13015bb8cf1ce9215e4
Jason Gors <jason.gors.work@gmail.com>
2016-02-28 04:16:43 +0000

    RF: add doc str and tidy up code

--- kafka/kafka_topic_setup.py
+++ kafka/kafka_topic_setup.py
@@ -3,17 +3,17 @@
 #----------------------------------------------------------------
 # Author: Jason Gors <jasonDOTgorsATgmail>
 # Creation Date: 01-26-2016
-# Purpose:
+# Purpose: create the kafka topics
 #----------------------------------------------------------------
-import subprocess
 
+import subprocess
 import sys, os
 parent_dir = os.path.dirname(os.getcwd())
 sys.path.append(parent_dir)
 import envir_vars
 
 # Kafka is started already (via peagus),
-# now need to create the topics to be used (just do on master):
+# now need to create the topics to be used (just do this on hdfs master):
 # NOTE this will create 1 topic per city, with one partition per topic
 replication_factor = 3
 partitions = 1
