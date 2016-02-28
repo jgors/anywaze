7f8ad1f4e605f8ce0aad70135e3c2e269460b5f8
Jason Gors <jason.gors.work@gmail.com>
2016-02-28 04:15:29 +0000

    RF: add doc str and tidy up code

--- kafka/kafka_producer.py
+++ kafka/kafka_producer.py
@@ -3,7 +3,7 @@
 #----------------------------------------------------------------
 # Author: Jason Gors <jasonDOTgorsATgmail>
 # Creation Date: 01-20-2016
-# Purpose:
+# Purpose: ingest raw data in flat files into kafka
 #----------------------------------------------------------------
 
 import sys
@@ -12,21 +12,16 @@ import kafka
 
 city = topic = sys.argv[1]
 
-# city = 'philly.txt'
-# topic = 'philly-test'
-# datafile = 'San_Francisco_old.txt'
-# topic = 'san-fran-small-wo-newline'
 # datafile = 'San_Francisco.txt'
-# topic = 'san-francisco'
+# city = topic = 'san-francisco'
 
 # connect to Kafka Cluster
-# cluster = kafka.KafkaClient("localhost:9092")     # for running the kafka producer on the same node as the master node
+# cluster = kafka.KafkaClient("localhost:9092")     # for running the kafka producer on the same node as hdfs master node
 # cluster = kafka.KafkaClient("ec2-52-89-106-226.us-west-2.compute.amazonaws.com:9092")
 kafka_nodes = ['{}:9092'.format(node) for node in ["52.89.106.226", '52.89.11.71', '52.34.130.78', '52.89.118.67']]
 cluster = kafka.KafkaClient(kafka_nodes)    # to send from somewhere remote into the aws kafka cluster
 
 prod = kafka.SimpleProducer(cluster, async=False)
-
 
 # produce msgs
 datafile = '/home/ubuntu/waze_data/{}.txt'.format(city)
